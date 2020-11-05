2852059cf68decd189201ab94afc3a042a0b6eb3
jc68261 <49455819+jc68261@users.noreply.github.com>
2020-11-05 15:40:50 -0500

    Update CreditCard.sql

--- SQL/CreditCard.sql
+++ SQL/CreditCard.sql
@@ -1,16 +1,14 @@
-CREATE DATABASE `data_credit_card` DEFAULT CHARACTER SET utf8mb4;
-
-USE `data_credit_card`;
+USE `merchandise`;
 
 DROP TABLE IF EXISTS `creditCard`;
 CREATE TABLE `creditCard` (
 	`CreditCardNo` char(16) NOT NULL DEFAULT '',
-	`Amount` decimal (5,2) NOT NULL DEFAULT '',
-	PRIMARY KEY (`CreditCardNo`),	
+	`Amount` decimal (5,2) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`CreditCardNo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 set autocommit=0;
-INSERT INTO `data_credit_card` VALUES (98456723401349761, 1000.00);
-INSERT INTO `data_credit_card` VALUES (001122334455667788, 500.00);
-INSERT INTO `data_credit_card` VALUES (1234567890254678, 100.00);
+INSERT INTO `creditCard` VALUES (98456723401349761, 1000.00);
+INSERT INTO `creditCard` VALUES (001122334455667788, 500.00);
+INSERT INTO `creditCard` VALUES (1234567890254678, 100.00);
 commit;
