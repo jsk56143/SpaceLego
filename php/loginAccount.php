49cdfe1575ff4f387ae954cc186fc43abe75e87a
jc68261 <49455819+jc68261@users.noreply.github.com>
2020-11-20 17:52:16 -0500

    Update loginAccount.php

--- php/loginAccount.php
+++ php/loginAccount.php
@@ -1,17 +1,20 @@
 <?php
 	include('accountDatabase.php');
-	include('../html/loginForm.html');
-	$username=(isset($_POST['UserName']) ? $_POST['UserName'] : '');
-	$password=(isset($_POST['Password']) ? $_POST['Password'] : '');
+	//$username=(isset($_POST['UserName']) ? $_POST['UserName'] : '');
+	//$password=(isset($_POST['Password']) ? $_POST['Password'] : '');
+	$username= $_POST['UserName'];
+	$password= $_POST['Password'];
 	
-	$query="SELECT * FROM account WHERE UserName='$username' AND WHERE Password='$password'";
+	$query="SELECT * FROM account WHERE UserName='$username' AND Password='$password'";
     $accounts = $db->query($query);
     
-    if (empty($accounts)) {
-        echo "Login failed.";
+    if ($accounts->rowCount() > 0) {
+		header("Location: index.php");
     }
     else {
-        echo "Redirecting you to your account page";
+		$status = 'Invalid Username or Password';
+        include('loginForm.php');
+		exit();
     }
 
 
