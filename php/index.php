c3008c99b72b0975d3d462d06ee0ac042352853e
jc68261 <49455819+jc68261@users.noreply.github.com>
2020-11-20 17:54:41 -0500

    Added files

---
+++ php/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Space Lego Shop</title>
+		<link rel="shortcut icon" href="../res/favicon.ico">
+		<link rel="normal" href="../styles/normalize.css">
+		<link rel="stylesheet" href="../styles/stylesheet.css">
+	</head>
+
+	<body>
+		<header> 
+			<a href="index.php"><img id = "logo" src="../res/logo.png" alt="Shop Logo"></a>
+			<a href="../html/addForm.html"><span class="login"> Register </span> <span class="login"> | </span> <a href="loginForm.php"><span class="login"> Log in </span>
+			<span id="welcome"> Welcome! </span> 
+			<a href="../html/Construction.html"><img src="../res/shopping_cart.png" id="cart"><span id="cartText">MY CART</span></a></li> 
+			<input type="search" id ="searchbar" name="searchbar" placeholder="Enter your search here... ">
+			<button type="submit" id="searchButton">
+		</header>
+
+	<nav id="nav_menu">
+			<ul>
+				<li><a href="index.php" id="currentPage">HOME</a></li>
+
+				<li><a href="#">SETS</a>
+					<ul>
+						<li><a href="../html/Construction.html">Space Police</a></li>
+						<li><a href="../html/Construction.html">Mars Mission</a></li>
+						<li><a href="../html/Construction.html">Galaxy Squad</a></li>
+						<li><a href="../html/Construction.html">Alien Conquest</a></li>
+						<li><a href="../html/Construction.html">Star Wars</a></li>
+					</ul>
+				</li>
+
+				<li><a href="../html/Construction.html">PREORDERS</a></li>
+				<li><a href="../html/faq.html">FAQ</a></li>
+				<li><a href="../html/contact.html">CONTACT US</a></li>
+			</ul>
+		</nav>
+
+		<aside id="leftColumn"> 
+			<h3> NEWEST RELEASES </h3>
+			<ul>
+				<a href=""><img src="../res/product1.jpg" alt="product1"> ITEM 1</a>
+				<a href=""><img src="../res/product2.jpg" alt="product2"> ITEM 2</a>
+				<a href=""><img src="../res/product3.jpg" alt="product3"> ITEM 3</a>
+			</ul>
+
+			<h3> BEST SELLERS </h3>
+			<ul>
+				<a href=""><img src="../res/product1.jpg" alt="product1"> ITEM 1</a>
+				<a href=""><img src="../res/product2.jpg" alt="product2"> ITEM 2</a>
+				<a href=""><img src="../res/product3.jpg" alt="product3"> ITEM 3</a>
+			</ul>
+		</aside>
+
+		<main>
+			<h3 id="hot"> WHAT'S HOT </h3>
+			<a href=""><img src="" alt="FRONT"></a>	
+		</main>
+
+		<footer> 
+			<p> &copy; 2020 Space Lego™ </p>
+		</footer>
+	</body>
+</html>
