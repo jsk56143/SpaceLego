8c6c83f1c70275029fa33cc3344d36e35ad89fb1
jsk56143 <jsk56143@uga.edu>
2020-11-22 14:58:20 -0500

    Removed image link for account creation

--- SQL/account.sql
+++ SQL/account.sql
@@ -6,13 +6,12 @@ CREATE TABLE `account` (
 	`UserName` char(50) NOT NULL DEFAULT '',
 	`Password` char(50) NOT NULL DEFAULT '',
 	`Email` char(60) NOT NULL DEFAULT '',
-	`Image` char(80) NOT NULL DEFAULT '',
 	`CreditCardNo` char(16) NOT NULL DEFAULT '',
 	PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 set autocommit=0;
-INSERT INTO `account` VALUES (1, 'user1', 'pass1', 'user1@email.com', 'https://i.ytimg.com/vi/DrwYZCN2__g/maxresdefault.jpg', 9845672340134976);
-INSERT INTO `account` VALUES (2, 'user2', 'pass2', 'user2@email.com', 'https://i.imgur.com/YCkjigb.png', 001122334455667788);
-INSERT INTO `account` VALUES (3, 'user3', 'pass3', 'user3@email.com', 'https://i.pinimg.com/564x/e2/ed/ba/e2edbaaccf19f5a77748044163c6ec94.jpg', 1234567890254678);
+INSERT INTO `account` VALUES (1, 'user1', 'pass1', 'user1@email.com', 9845672340134976);
+INSERT INTO `account` VALUES (2, 'user2', 'pass2', 'user2@email.com', 001122334455667788);
+INSERT INTO `account` VALUES (3, 'user3', 'pass3', 'user3@email.com', 1234567890254678);
 commit;
--- php/addAccount.php
+++ php/addAccount.php
@@ -4,12 +4,11 @@
 	$username=(isset($_POST['UserName']) ? $_POST['UserName'] : '');
 	$password=(isset($_POST['Password']) ? $_POST['Password'] : '');
     $email=(isset($_POST['Email']) ? $_POST['Email'] : '');
-    $image=(isset($_POST['Image']) ? $_POST['Image'] : '');
     $creditcardno=(isset($_POST['CreditCardNo']) ? $_POST['CreditCardNo'] : '');
 	
 	$query="INSERT INTO account
-		(UserName, Password, Email, Image, CreditCardNo) VALUES
-		('$username', '$password', '$email', '$image', '$creditcardno')";
+		(UserName, Password, Email, CreditCardNo) VALUES
+		('$username', '$password', '$email', '$creditcardno')";
 	$db->exec($query);
 	
 	header('Location: index.php');
