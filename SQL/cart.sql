5bfd9a7c96b8f143bc8d6c968e00679898c0669a
jc68261 <49455819+jc68261@users.noreply.github.com>
2020-11-22 19:54:41 -0500

    Reupdating files

--- SQL/cart.sql
+++ SQL/cart.sql
@@ -3,8 +3,11 @@ USE `merchandise`;
 DROP TABLE IF EXISTS `cart`;
 CREATE TABLE `cart` (
 	`ID` int NOT NULL AUTO_INCREMENT,
-	`ProductCount` int NOT NULL DEFAULT '0',
-	PRIMARY KEY (`ID`)	
+	`UserID` int NOT NULL DEFAULT '0',
+	`ProductID` int NOT NULL DEFAULT '0',
+	PRIMARY KEY (`ID`),
+	FOREIGN KEY (`UserID`) REFERENCES `account` (`ID`),
+	FOREIGN KEY (`ProductID`) REFERENCES `product` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 set autocommit=0;
