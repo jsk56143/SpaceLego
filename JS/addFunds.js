a45f3b9287312a72df8bbf0a07f8e28b5a161829
jc68261 <49455819+jc68261@users.noreply.github.com>
2020-11-24 12:05:48 -0500

    Changed add Funds

--- JS/addFunds.js
+++ JS/addFunds.js
@@ -3,29 +3,21 @@ var $ = function(id) {
 }
 
 var checkCredentials = function() {
-	var username = $("username").value;
-	
 	var fund = $("fund").value;
 	
 	var noError = true;
 	
-	var error1 = "*";
 	var error2 = "*";
-	
-	if (username === "") {
-		error1 = "Username must NOT be empty.";
-		noError = false;
-	}
+
 	if (isNaN(fund) || fund === "" || fund <= 0) {
 		error2 = "Fund must be numeric, not empty, and be greater than zero.";
 		noError = false;	
 	}
 	if (noError == true) {
-		error1 = "*";
 		error2 = "*";
 		document.addFundForm.submit();
 	}
-	$("error1").firstChild.nodeValue = error1;
+
 	$("error2").firstChild.nodeValue = error2;
 }
 	
