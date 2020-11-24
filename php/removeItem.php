f9226ce68754790eaa24c6cdfed086136c92117d
jsk56143 <jsk56143@uga.edu>
2020-11-24 08:26:34 -0500

    Fixed bug of remove button removing duplicate items

--- php/removeItem.php
+++ php/removeItem.php
@@ -5,7 +5,7 @@
 	
 	$productID=(isset($_POST['hiddenRemoveItem']) ? $_POST['hiddenRemoveItem'] : '');
 	
-	$query="DELETE FROM cart WHERE ProductID=$productID";
+	$query="DELETE FROM cart WHERE ProductID=$productID LIMIT 1";
 	$db->exec($query);
 	
 	header('Location: cart_view.php');
