d5e279e2894290b99ff2628023ebb59bf1797a02
Duy Nguyen <49450788+OmegaSJX@users.noreply.github.com>
2020-11-23 19:31:21 -0500

    takes cc amount/new query for cc database

--- php/addAccount.php
+++ php/addAccount.php
@@ -5,11 +5,16 @@
 	$password=(isset($_POST['Password']) ? $_POST['Password'] : '');
     $email=(isset($_POST['Email']) ? $_POST['Email'] : '');
     $creditcardno=(isset($_POST['CreditCardNo']) ? $_POST['CreditCardNo'] : '');
-	
+	$creditcardamount=(isset($_POST['Amount']) ? $_POST['Amount'] : '');
 	$query="INSERT INTO account
 		(UserName, Password, Email, CreditCardNo) VALUES
 		('$username', '$password', '$email', '$creditcardno')";
+		
+	$cardquery="INSERT INTO creditcard
+		(CreditCardNo, Amount) VALUES
+		('$creditcardno', '$creditcardamount')";
 	$db->exec($query);
+	$db->exec($cardquery);
 	
 	header('Location: index.php');
 ?>
